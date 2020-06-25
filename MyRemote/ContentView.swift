012216a405b9638b62a80a5cf60f97a43f824975
Alex DeMeo <alex.demeo129@gmail.com>
2020-06-25 15:31:55 -0700

    Initial Commit

---
+++ MyRemote/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MyRemote
+//
+//  Created by Alex DeMeo on 6/25/20.
+//  Copyright © 2020 Alex DeMeo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
